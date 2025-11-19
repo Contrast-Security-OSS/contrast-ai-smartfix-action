37b9f8af15b20ff5c1fba20d588c6e7494c192bc
Jacob Mages-Haskins <jacob.mages-haskins@contrastsecurity.coms>
2025-11-19 10:56:19 -0500

    Bump version to v1.0.11

--- src/config.py
+++ src/config.py
@@ -49,7 +49,7 @@ class Config:
         self.testing = testing
 
         # --- Preset ---
-        self.VERSION = "v1.0.10"
+        self.VERSION = "v1.0.11"
         self.USER_AGENT = f"contrast-smart-fix {self.VERSION}"
 
         # --- Core Settings ---
